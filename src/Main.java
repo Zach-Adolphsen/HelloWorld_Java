11bdd8f7c1b3090d64e81c5cda73c0fda6b359d0
Zach-Adolphsen <zladolphsen@gmail.com>
2024-12-03 18:52:12 -0600

    Random Code for commit

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,14 @@ public class Main {
         for(int i =1; i<=5; i++){
             System.out.println("i = " + i);
         }
+
+        boolean test = true;
+        for(int i = 1; i <= 9; i++){
+            System.out.println("i = " + i);
+            System.out.println("test = " + test);
+
+            if(i == 4) break;
+        }
     }
+
 }
