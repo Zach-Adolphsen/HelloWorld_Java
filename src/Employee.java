469a1934187a2fd6bc4613012f5369f4e7608b1f
Zach-Adolphsen <zladolphsen@gmail.com>
2024-12-04 10:59:07 -0600

    Creation of Employee class

---
+++ src/Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+
+    public String name;
+    public String title;
+    public double salary;
+
+    public Employee(String name, String title, double salary){
+        this.name = name;
+        this.title = title;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public String toString(){
+        return "Name: " + name + "Title: " + title + "Salary: " + salary;
+    }
+
+
+
+}
