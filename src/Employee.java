ea7393d2280b05289855259e3f9aecd87da34f83
Zach-Adolphsen <zladolphsen@gmail.com>
2024-12-12 15:44:42 -0600

    Created setter methods for Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -13,13 +13,24 @@ public class Employee {
     public String getName(){
         return name;
     }
+    public void setName(String name){
+        this.name=name;
+    }
 
     public String getTitle(){
         return title;
     }
 
+    public void setTitle(String title){
+        this.title=title;
+    }
+
     public double getSalary(){
         return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary=salary;
     }
 
     public String toString(){
